0ca586ce448ef71a885a56c4396d1a400869d29f
Tushar <112742512+Tushar-18@users.noreply.github.com>
2023-08-14 11:20:21 +0530

    admin

--- resources/views/admin/admin-product.blade.php
+++ resources/views/admin/admin-product.blade.php
@@ -9,12 +9,10 @@
          <thead class="bg-gray-50">
              <tr>
                  <th scope="col" class="px-6 py-4 font-medium text-gray-900">Product Images</th>
-                 <th scope="col" class="px-6 py-4 font-medium text-gray-900">Product Name</th>
                  <th scope="col" class="px-6 py-4 font-medium text-gray-900">Product Price</th>
-                 <th scope="col" class="px-6 py-4 font-medium text-gray-900">Product Category</th>
-                 <th scope="col" class="px-6 py-4 font-medium text-gray-900">Product brand</th>
-                 <th scope="col" class="px-6 py-4 font-medium text-gray-900">Product Quantity</th>
+                 <th scope="col" class="px-6 py-4 font-medium text-gray-900">Product quantity</th>
                  <th scope="col" class="px-6 py-4 font-medium text-gray-900">Product Description</th>
+                 
                  <th scope="col" class="px-6 py-4 font-medium text-gray-900">State</th>
                  <th scope="col" class="px-6 py-4 font-medium text-gray-900">Delete</th>
                  <th scope="col" class="px-6 py-4 font-medium text-gray-900">Edit</th>
@@ -37,36 +35,38 @@
                      </div>
                  </th>
                  <td class="px-6 py-4">
-                     {{-- <span>{{$d['user_pwd']}}</span> --}}
+                     <span>₹{{$d['product_price']}}</span>
                  </td>
                  <td class="px-6 py-4">
-                     {{-- <span>{{$d['user_number']}}</span> --}}
+                     <span>{{$d['product_quantity']}}</span>
                  </td>
                  <td class="px-6 py-4">
-                     {{-- <span>{{$d['user_address']}}</span> --}}
+                     <textarea name="" id="" cols="30" rows="2">
+                        {{$d['product_description']}}
+                     </textarea>
                  </td>
-                 <td class="px-6 py-4">
-                     {{-- <span>{{$d['user_city']}}</span> --}}
-                 </td>
-                 <td class="px-6 py-4">
-                     {{-- <span>{{$d['user_states']}}</span> --}}
-                 </td>
-                 <td class="px-6 py-4">
-                     {{-- <span>{{$d['user_pincode']}}</span> --}}
-                 </td>
+                
                  <td class="px-6 py-4" style="width: 200px">
-                     {{-- <span>{{$d['created_at']}}</span> --}}
-                 </td>
-                 <td class="px-6 py-4">
                      <span
                          class="inline-flex items-center gap-1 rounded-full bg-red-50 px-2 py-1 text-xs font-semibold text-red-600">
                          <span class="h-1.5 w-1.5 rounded-full bg-red-600"></span>
-                         {{-- {{$d['user_status']}} --}}
+                         {{$d['product_status']}}
                      </span>
                  </td>
+                 <td class="px-8 py-4" style="width: 200px">
+                    <a x-data="{ tooltip: 'Delete' }" href="#">
+                             <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5" stroke="currentColor" class="h-6 w-6 stroke-red-600"
+                                 x-tooltip="tooltip">
+                                 <path stroke-linecap="round" stroke-linejoin="round"
+                                     d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                             </svg>
+                         </a>
+                 </td>
+                 
                  {{-- <td class="px-6 py-4">{{$d['user_role']}}</td> --}}
                  
-                 <td class="px-6 py-4">
+                 {{-- <td class="px-6 py-4">
                      <div class="flex justify-end gap-4">
                          <a x-data="{ tooltip: 'Delete' }" href="#">
                              <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -77,7 +77,7 @@
                              </svg>
                          </a>
                      </div>
-                 </td>
+                 </td> --}}
                  <td class="px-6 py-4">
                      <div class="flex justify-end gap-4">
                          <a x-data="{ tooltip: 'Delete' }" href="#">
