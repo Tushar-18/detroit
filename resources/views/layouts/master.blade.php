586bfcf3f7413047483cdde3983b729801930830
HarshitNananiya <hnananiya813@rku.ac.in>
2023-09-16 18:19:05 +0530

    home connect

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@
         <div class="logo"><img src="{{URL::to('/')}}/Images/black-logo.png" alt="error"></div>
         <nav class="nav">
             <ul class="c1">
-                <li><a href="">Home</a></li>
+                <li><a href="/">Home</a></li>
                 <li style="display: flex;">PC Component <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-7">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                         </svg></span>
