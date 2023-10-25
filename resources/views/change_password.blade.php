11262f3db2bde90aeb4653c187567881f5b10c15
HarshitNananiya <hnananiya813@rku.ac.in>
2023-10-25 09:09:00 +0530

    add

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -17,37 +17,36 @@
             <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
                 aria-hidden="true">
             </div>
-            <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20 p-2">
+            <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div class="sm:col-span-2">
-                        <label for="company" class="block text-sm font-semibold leading-6 text-gray-900">Name</label>
+                        <label for="company" class="block text-sm font-semibold leading-6 text-gray-900">Old Password</label>
                         <div class="mt-2.5">
-                            <input type="password" name="old_pwd" id="company" autocomplete="organization"
+                            <input type="Password" name="company" id="company" autocomplete="organization"
                                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
                     <div class="sm:col-span-2">
-                        <label for="company" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+                        <label for="company" class="block text-sm font-semibold leading-6 text-gray-900">New password</label>
                         <div class="mt-2.5">
-                            <input type="password" name="new_pwd" id="company" autocomplete="organization"
+                            <input type="Password" name="company" id="company" autocomplete="organization"
                                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Address</label>
+                        <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Confirm Password</label>
                         <div class="mt-2.5">
-                         <input type="password" name="confirm_password" id="company" autocomplete="organization"
+                         <input type="Password" name="company" id="company" autocomplete="organization"
                          class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
-                    
-               
+                </div>
+               <br>
                 <div >
                     <button type="submit"
-                        class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+                        class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">change Password</button>
                 </div>
-                <label for="" class="text-blue-600"><a href="change_password">change password</a></label>
             </form>
         </div>
     @endsection
